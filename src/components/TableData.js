6c3a6d9e9ab5931c1ad56f94943654992a65b39f
Nikita Legkiy Maxakov <iamlegkiy@gmail.com>
2022-08-31 11:15:36 +0800

    replace data and out

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,8 +1,8 @@
-import React from "react";
-import region from "./boilerScript/regions.json";
-import script1 from "./boilerScript/script1.json";
-import script2 from "./boilerScript/script2.json";
-import RegionScript from "./RegionScript";
+import React from 'react';
+import region from './boilerScript/regions.json';
+import script1 from './boilerScript/script1.json';
+import script2 from './boilerScript/script2.json';
+import RegionScript from './RegionScript';
 
 function TableData(props) {
   const scripts = [script1, script2];
@@ -14,22 +14,22 @@ function TableData(props) {
           <tr
             key={curState.id}
             className={`
-                  ${curState.total ? "reg-total" : "reg-row"}
+                  ${curState.total ? 'reg-total' : 'reg-row'}
                 `}
           >
             <td className="reg-name" rowSpan={2}>
               {curState.localName}
             </td>
-            <td>{curState.coal}</td>
-            <td>{curState.wood}</td>
-            <td>{curState.oil}</td>
-            <td>{curState.gas}</td>
-            <td>{curState.electric}</td>
-            <td>{curState.solid}</td>
-            <td>{curState.sulfur}</td>
-            <td>{curState.nitrogen}</td>
-            <td>{curState.carbon}</td>
-            <td>{curState.ash}</td>
+            <td>{curState.coal === 0 ? '-' : curState.coal}</td>
+            <td>{curState.wood === 0 ? '-' : curState.wood}</td>
+            <td>{curState.oil === 0 ? '-' : curState.oil}</td>
+            <td>{curState.gas === 0 ? '-' : curState.gas}</td>
+            <td>{curState.electric === 0 ? '-' : curState.electric}</td>
+            <td>{curState.solid === 0 ? '-' : curState.solid}</td>
+            <td>{curState.sulfur === 0 ? '-' : curState.sulfur}</td>
+            <td>{curState.nitrogen === 0 ? '-' : curState.nitrogen}</td>
+            <td>{curState.carbon === 0 ? '-' : curState.carbon}</td>
+            <td>{curState.ash === 0 ? '-' : curState.ash}</td>
           </tr>
           {/* {props.scriptIndex} */}
           {props.scriptIndex !== null ? (
