b9730bdbec0deb3f936b129f6e71ad01fa707d04
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-10-05 11:47:20 +0800

    added map with solar and wind

--- src/components/YaMap.js
+++ src/components/YaMap.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useEffect } from 'react';
 
-export default function YaMap({scriptIndex}) {
+export default function YaMap({ scriptIndex }) {
   const urls = [
+    //2 map  with baikal ecolog border
     'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9652247e6b84ac970072b95d77fbe77dca40ca7410991b61d9a5ed0e96fab8f6&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
     'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9652247e6b84ac970072b95d77fbe77dca40ca7410991b61d9a5ed0e96fab8f6&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+    //map with LAP
     'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A8cb571e2838a57fe3900df867bc6ea8ccb01643e902d92b8ebfc8a5a00a30528&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
   ];
 
@@ -12,6 +14,7 @@ export default function YaMap({scriptIndex}) {
     const mapWay = document.getElementById('ya-map');
     mapWay.innerHTML = '';
     const script = document.createElement('script');
+    script.type = 'text/javascript';
     script.src =
       urls[scriptIndex] ||
       'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9652247e6b84ac970072b95d77fbe77dca40ca7410991b61d9a5ed0e96fab8f6&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true';
@@ -27,4 +30,4 @@ export default function YaMap({scriptIndex}) {
       <div className="ya-map" id="ya-map"></div>
     </div>
   );
-};
+}
--- src/components/mainBoxFolder/MainOnlyMap.js
+++ src/components/mainBoxFolder/MainOnlyMap.js
@@ -1,15 +1,18 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 
-export default function MainOnlyMap({scriptIndex}) {
-  const urls=[
-    "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A4a221686dbf5fd263c2c7cf067e14d9cf30e4a511542453c1b9c0d6a074f3373&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true",
-  ]
+export default function MainOnlyMap({ scriptIndex }) {
+  const urls = [
+    //wood map
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A4a221686dbf5fd263c2c7cf067e14d9cf30e4a511542453c1b9c0d6a074f3373&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+    //solar and wind map
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A37d77bd26d859f7f3c97b9ba7a643ea9adde78afa6ab3567825212b16dfd74ea&amp;width=760&amp;height=513&amp;lang=ru_RU&amp;scroll=true',
+  ];
   useEffect(() => {
     const mapWay = document.getElementById('ya-map');
     mapWay.innerHTML = '';
     const script = document.createElement('script');
-    script.src =
-      urls[scriptIndex-3]
+    script.type = 'text/javascript';
+    script.src = urls[scriptIndex - 3];
     script.async = true;
     mapWay.appendChild(script);
   });
@@ -19,8 +22,8 @@ export default function MainOnlyMap({scriptIndex}) {
       <h2 className="banner">
         Расположение объектов энергетической инфраструктуры
       </h2>
-      <div className='ya-map-box'>
-      <div className="ya-map" id="ya-map"></div>
+      <div className="ya-map-box">
+        <div className="ya-map" id="ya-map"></div>
       </div>
     </div>
   );
