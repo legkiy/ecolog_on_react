4f963242e88ebe6c705757fe1a5f9096ca1495a9
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-11-07 12:52:39 +0800

    added new map for "now script"

--- src/components/YaMap.js
+++ src/components/YaMap.js
@@ -3,8 +3,9 @@ import { useEffect } from 'react';
 
 export default function YaMap({ scriptIndex }) {
   const urls = [
-    //2 map  with baikal ecolog border
-    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9652247e6b84ac970072b95d77fbe77dca40ca7410991b61d9a5ed0e96fab8f6&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+    //now script with cherem
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A79adc590518903f186b6ea6d945d6221fc5c43469c28a544ab123b83c9e67e28&amp;width=447&amp;height=445&amp;lang=ru_RU&amp;scroll=true',
+    //coal script
     'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9652247e6b84ac970072b95d77fbe77dca40ca7410991b61d9a5ed0e96fab8f6&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
     //map with LAP
     'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A8cb571e2838a57fe3900df867bc6ea8ccb01643e902d92b8ebfc8a5a00a30528&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
@@ -15,7 +16,7 @@ export default function YaMap({ scriptIndex }) {
     mapWay.innerHTML = '';
     const script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = urls[scriptIndex] || urls[0];
+    script.src = urls[scriptIndex] || urls[1];
     script.async = true;
     mapWay.appendChild(script);
   });
