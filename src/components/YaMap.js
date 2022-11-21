769cf75198579e918c4578cc09b9a70206d72b5a
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-11-21 14:50:07 +0800

    new now-script map

--- src/components/YaMap.js
+++ src/components/YaMap.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 export default function YaMap({ scriptIndex }) {
   const urls = [
     //now script with cherem
-    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A79adc590518903f186b6ea6d945d6221fc5c43469c28a544ab123b83c9e67e28&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A348ab2921a417af486e1c724d73b0ff8f0b8af9d2e9d6dc77cb9b93eba8122cb&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
     //coal script
     'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A964b525d13d22f3e62d0094cd7e27e1c78a69e184fc959859336134baf6a4631&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
     //map with LAP
