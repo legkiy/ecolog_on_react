c6766353b6dae81d33270cf0e8d75afe5457f4fe
Nikita Legkiy Maxakov <iamlegkiy@gmail.com>
2022-11-25 18:26:50 +0800

    added back btn

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from '../img/logo-white.png';
+import { IoArrowBackOutline } from 'react-icons/io5';
 
 export default function Head() {
   return (
     <header className="head">
-      <div className="box-80">
-        <a href="https://isem.irk.ru/" id="isem-link">
+      <div className="box-80 links">
+        <a href="https://isem.irk.ru/solutions/" className="link">
+          <IoArrowBackOutline />
+          Назад
+        </a>
+        <a href="https://isem.irk.ru/" className="link">
           <img src={logo} alt="Логотип ИСЭМ"></img>
           <h2 className="head-text">
             Федеральное государственное бюджетное учреждение науки
