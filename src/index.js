56a2387a7f5a0642fd8361d6058d86a1eea501cf
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2022-12-27 12:14:17 +0800

    start remake all app

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import * as ReactDOMClient from "react-dom/client";
-import App from "./App";
-import "./css/normalize.css"
-import "./css/main-style.css"
-
-const app = ReactDOMClient.createRoot(document.getElementById("root"));
-app.render(<App />);
