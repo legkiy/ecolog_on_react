8f48c9b84bb02a75522c12398747ad871b80931a
Nikita Maxakov (lab64PC) <iamlegkiy@gmail.com>
2023-01-11 11:15:22 +0800

    added yandex map

---
+++ src/components/YandexMap/YandexMap.tsx
@@ -0,0 +1,43 @@
+import './YandexMap.scss';
+import { useEffect } from 'react';
+import { RootState } from '../../app/store';
+import { useSelector } from 'react-redux';
+
+interface IPropsYandexMap {}
+
+const YandexMap = ({}: IPropsYandexMap) => {
+  const caseIndex = useSelector((state: RootState) => state.case.caseIndex);
+
+  const urls = [
+    //now script with cherem
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A348ab2921a417af486e1c724d73b0ff8f0b8af9d2e9d6dc77cb9b93eba8122cb&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+    //coal script
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ad23699d514f7cbf0166cf175a2c869a086c4ec4eb01d4d6db923ea3e45a7bbcb&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+    //electro script
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A589b3831b444362c71bee9acd47159602001b3c2a8755c949c6539bf4c106236&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+    //gas script
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A752e951aaa597af01c5a6e9fcf7774908c7fdde94a7994011ef16124b910fdb5&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+    //renew energy
+    'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ad45033bef372af85fe0bedbf969feb6b64fc3687248356d0a89376585a7e949e&amp;width=100%&amp;height=100%&amp;lang=ru_RU&amp;scroll=true',
+  ];
+
+  useEffect(() => {
+    const mapWay = document.getElementById('ya-map');
+    mapWay!.innerHTML = '';
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = urls[caseIndex] || urls[1];
+    script.async = true;
+    mapWay!.appendChild(script);
+  });
+
+  return (
+    <div className="yandexMap">
+      <div>
+        <h2 className="banner">Расположение объектов энергетической инфраструктуры</h2>
+      </div>
+      <div className="ya-map" id="ya-map"></div>
+    </div>
+  );
+};
+export default YandexMap;
